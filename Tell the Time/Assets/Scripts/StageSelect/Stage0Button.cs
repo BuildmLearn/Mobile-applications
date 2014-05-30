62dcff497f3ee7471fa0a6bdb90e8869158dc073
Kelvin Khoo <kelvin.khoo.12@ucl.ac.uk>
2014-05-30 07:16:15 +0100

    Add back the files

---
+++ Tell the Time/Assets/Scripts/StageSelect/Stage0Button.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Stage0Button : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButton(0)) {
+			Application.LoadLevel ("stage_loading");
+		}
+	}
+}

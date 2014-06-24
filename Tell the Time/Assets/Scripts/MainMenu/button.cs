a793577f0f8d10258aed0bc5db94ba259a8d5599
Kelvin Khoo <kelvin.khoo.12@ucl.ac.uk>
2014-06-25 02:14:23 +0800

    Fixed stage one and two logic problem, added another 3 stages

---
+++ Tell the Time/Assets/Scripts/MainMenu/button.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class button : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+				if (Input.touches.Length <= 0) {
+						// if no touches
+				} else {
+
+						for (int i=0; i<Input.touchCount; i++) {
+								//execute current touch on screen
+								if (this.guiTexture.HitTest (Input.GetTouch (i).position)) {
+										//if current touch hit our guitexture run this code
+										if (Input.GetTouch (i).phase == TouchPhase.Began) {
+												switch (this.name) {
+												case "PlayButton":
+														Application.LoadLevel ("StageSelect");
+														break;
+												case "StatButton":
+														break;
+												case "ConfigButton":
+														break;
+												}
+												//Debug.Log("The touch is down " + this.name);
+					
+										}
+
+								}
+						}
+				}
+		}
+}
---
+++ Tell the Time/Assets/Scripts/Stage5/Stage5Controller.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Stage5Controller : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape)) { Application.LoadLevel("StageSelect"); }
+
+	}
+}

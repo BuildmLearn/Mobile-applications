497e15ddadb0c348c46884ee6f9e8f63ef5be2b5
Kelvin Khoo <kelvin.khoo.12@ucl.ac.uk>
2014-06-25 13:48:26 +0800

    Fixed game logic problem for stage 3,4,5,6

---
+++ Tell the Time/Assets/Scripts/MainMenu/ButtonScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonScript : MonoBehaviour {
+
+	public Texture playButtonTexture;
+	public Texture statButtonTexture;
+	public Texture configButtonTexture;
+	
+	void OnGUI() {
+		//GUI.backgroundColor = Color.clear;
+
+		if (!playButtonTexture) {
+			Debug.LogError("Please assign a texture on the inspector");
+			return;
+		}
+		Rect playBtnRect = new Rect(Screen.width/2-Screen.width/6, (4.0f/6.5f)*Screen.height, Screen.width/3,Screen.height/12);
+		if(GUI.Button(playBtnRect,playButtonTexture))
+			Application.LoadLevel ("StageSelect");
+
+		Rect statBtnRect = new Rect(Screen.width/2-Screen.width/6, (4.0f/6.5f)*Screen.height+Screen.height/12+10, Screen.width/3,Screen.height/12);
+		Rect configBtnRect = new Rect(Screen.width/2-Screen.width/6, (4.0f/6.5f)*Screen.height+2*Screen.height/12+20, Screen.width/3,Screen.height/12);
+
+		GUI.Button (statBtnRect, statButtonTexture);
+		GUI.Button (configBtnRect, configButtonTexture);
+
+		// To quit the application.
+		if (Input.GetKeyDown(KeyCode.Escape)) { Application.Quit(); }
+		
+
+		//GUI.DrawTexture (playBtnRect, playButtonTexture);
+
+		//if(GUI.Button(playBtnRect,playButtonTexture))
+		//	Application.LoadLevel ("StageSelect");
+
+		
+	}
+
+}
---
+++ Tell the Time/Assets/Scripts/StageSelect/StageSelectButtonControl.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StageSelectButtonControl : MonoBehaviour {
+	bool touched = false;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		if (Input.GetKeyDown(KeyCode.Escape)) { Application.LoadLevel("mainMenu"); }
+
+		Vector3 pos = Camera.main.ScreenToWorldPoint (Input.GetTouch(0).position);
+		//Debug.Log (Input.mousePosition);
+		RaycastHit2D hit = Physics2D.Raycast(pos, Vector2.zero);
+		if (hit != null && hit.collider != null) 
+		{
+			switch(hit.collider.name)
+			{
+			case "Stage0":
+				hit.collider.name = "";
+				Debug.Log ("hit stage0");
+				Application.LoadLevel ("stage0_loading");
+				break;
+			case "Stage1":
+				hit.collider.name = "";
+				Debug.Log ("hit stage1");
+				Application.LoadLevel ("stage1_loading");
+				break;
+			case "Stage2":
+				hit.collider.name = "";
+				Debug.Log ("hit stage2");
+				Application.LoadLevel ("stage2_loading");
+				break;
+			case "Stage3":
+				hit.collider.name = "";
+				Debug.Log ("hit stage3");
+				Application.LoadLevel ("stage3_loading");
+				break;
+			case "Stage4":
+				hit.collider.name = "";
+				Debug.Log ("hit stage4");
+				Application.LoadLevel ("stage4_loading");
+				break;
+			case "Stage5":
+				hit.collider.name = "";
+				Debug.Log ("hit stage5");
+				Application.LoadLevel ("stage5_loading");
+				break;
+			}
+
+		}
+		if (Input.GetKeyDown(KeyCode.Escape)) { 
+			hit.collider.name = "";
+			Application.LoadLevel("mainMenu"); }
+
+	}
+}

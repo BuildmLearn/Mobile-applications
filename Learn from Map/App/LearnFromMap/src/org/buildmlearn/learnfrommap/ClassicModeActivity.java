0b34e5f53bcc7f265a73ea43234fbcd569f18d07
Abhishek Batra <abhishekbatra1993@gmail.com>
2014-07-08 02:07:23 +0530

    Added countries Classic Mode

--- Learn from Map/App/LearnFromMap/src/org/buildmlearn/learnfrommap/ClassicModeActivity.java
+++ Learn from Map/App/LearnFromMap/src/org/buildmlearn/learnfrommap/ClassicModeActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class ClassicModeActivity extends ActionBarActivity {
 
@@ -11,6 +13,14 @@ public class ClassicModeActivity extends ActionBarActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_classic_mode);
+		Spinner spinner = (Spinner) findViewById(R.id.classic_spinner1);
+		// Create an ArrayAdapter using the string array and a default spinner layout
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+		        R.array.countries_array, android.R.layout.simple_spinner_item);
+		// Specify the layout to use when the list of choices appears
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		// Apply the adapter to the spinner
+		spinner.setAdapter(adapter);
 
 	}
 

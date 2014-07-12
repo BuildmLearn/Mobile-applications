46fdaa1af6fae5fe6b49eee1909dc45b7c173d82
Kelvin Khoo <kelvin.khoo.12@ucl.ac.uk>
2014-07-12 23:19:24 +0800

    added scoring, refining Main UI, added answer check for stage 2

---
+++ Tell the Time/Assets/Scripts/Loading/LoadingButton.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadingButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			Application.LoadLevel("stage0");		
+		}
+	}
+}
---
+++ Tell the Time/Assets/Scripts/Stage2/RandomTime.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomTime : MonoBehaviour {
+	public GameObject longHand,shortHand;	
+	int time;
+	// Use this for initialization
+	void Start () {
+		time = (int)Random.Range (1.0F, 11.0F);
+		shortHand.transform.Rotate (0, 0, time*-30);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

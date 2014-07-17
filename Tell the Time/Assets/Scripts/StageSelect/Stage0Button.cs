c973d447f7dd430bd252f2635608f27f56b66660
Kelvin Khoo <kelvin.khoo.12@ucl.ac.uk>
2014-07-17 09:20:14 +0800

    Changed stage menu layout for release

---
+++ Tell the Time/Assets/Scripts/StageSelect/Stage0Button.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Stage0Button : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		RaycastHit2D hit = Physics2D.Raycast(pos, Vector2.zero);
+		if (hit != null && hit.collider != null) 
+		{
+			Debug.Log("I'm Hitting " + hit.collider.name);
+		}
+	}
+
+}

1143029b91c7e499e4cd796905ad5d52a3d71be3
Kelvin Khoo <kelvin.khoo.12@ucl.ac.uk>
2014-07-18 15:20:50 +0800

    Fix to minor error on touching the screen

---
+++ Tell the Time/Assets/Scripts/About/AboutScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AboutScript : MonoBehaviour {
+
+	string content = "'Tell the Time' is an educative game that teaches kids about the basic concepts of time (both analog and digital). The application helps kids learn to say time much quickly and in a fun way by using an interactive analog clock";
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape)) 
+		{ 
+			Application.LoadLevel("mainMenu"); 
+		}
+	}
+	
+}

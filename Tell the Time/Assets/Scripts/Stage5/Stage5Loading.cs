beaccbb491c57ab58f2bbce84c1d872d048cd279
Kelvin Khoo <kelvin.khoo.12@ucl.ac.uk>
2014-07-29 18:43:14 +0800

    Fixed gui for stage 3

---
+++ Tell the Time/Assets/Scripts/Stage5/Stage5Loading.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Stage5Loading : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			Application.LoadLevel("stage5");		
+		}
+	}
+}

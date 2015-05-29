471fe0f855c5d66bef0912b4c9e27ff85ae3c323
souljaboy764 <vigneshprasad141@gmail.com>
2015-05-30 00:56:24 +0530

    initial commit with splash screen

---
+++ Practice Handwriting/source-code/PracticeHandwriting/app/src/main/java/org/buildmlearn/practicehandwriting/SplashActivity.java
@@ -0,0 +1,18 @@
+package org.buildmlearn.practicehandwriting;
+
+import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+
+}

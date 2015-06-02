337bd17faa65ef91cfd851b2d9e8525da2e7e846
souljaboy764 <vigneshprasad141@gmail.com>
2015-06-03 00:11:59 +0530

    Character and Word set defined

--- Practice Handwriting/source-code/PracticeHandwriting/app/src/main/java/org/buildmlearn/practicehandwriting/SplashActivity.java
+++ Practice Handwriting/source-code/PracticeHandwriting/app/src/main/java/org/buildmlearn/practicehandwriting/SplashActivity.java
@@ -1,6 +1,7 @@
 package org.buildmlearn.practicehandwriting;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -13,6 +14,7 @@ public class SplashActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        //TODO pre processing if any
     }
 
 }

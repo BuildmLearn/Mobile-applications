5cd26018f2eaac5f8d539e09890ac058cc84bbfa
souljaboy764 <vigneshprasad141@gmail.com>
2015-06-06 04:26:59 +0400

    Made Screen to choose modes

---
+++ Practice Handwriting/source-code/PracticeHandwriting/app/src/main/java/org/buildmlearn/practicehandwriting/MainMenuActivity.java
@@ -0,0 +1,15 @@
+package org.buildmlearn.practicehandwriting;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class MainMenuActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+        //TODO define onClick listeners for layouts and add animation
+    }
+}

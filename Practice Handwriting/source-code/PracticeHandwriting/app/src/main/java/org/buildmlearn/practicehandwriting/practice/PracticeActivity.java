5102cec23b551314489f4785b27813645ec30825
souljaboy764 <vigneshprasad141@gmail.com>
2015-06-09 13:40:57 +0400

    added TTS and reset functionality to PracticeActivity

--- Practice Handwriting/source-code/PracticeHandwriting/app/src/main/java/org/buildmlearn/practicehandwriting/practice/PracticeActivity.java
+++ Practice Handwriting/source-code/PracticeHandwriting/app/src/main/java/org/buildmlearn/practicehandwriting/practice/PracticeActivity.java
@@ -3,29 +3,47 @@ package org.buildmlearn.practicehandwriting.practice;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.os.Build;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.view.View;
 import android.widget.TextView;
 
 import org.buildmlearn.practicehandwriting.R;
+import org.buildmlearn.practicehandwriting.SplashActivity;
 
-public class PracticeActivity extends Activity {
+public class PracticeActivity extends Activity {//implements TextToSpeech.OnInitListener{
 
     //TODO add button animation and functionality, make options menu with next and previous, time dependant vibrations, error toast
+
     private Bitmap drawViewBitmap;
+    private DrawingView drawView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practice);
 
         Intent intent = getIntent();
-
-        DrawingView draw = (DrawingView) findViewById(R.id.drawing);
+        String practice_string = intent.getStringExtra(getResources().getString(R.string.practice_string));
+        drawView = (DrawingView) findViewById(R.id.drawing);
         TextView canvasText = (TextView) findViewById(R.id.canvas);
 
-        canvasText.setText(intent.getStringExtra(getResources().getString(R.string.practice_string)));
+        canvasText.setText(practice_string);
         drawViewBitmap = takeScreenshot(R.id.canvas);
-        draw.setbitmap(drawViewBitmap);
+        drawView.setBitmap(drawViewBitmap);
+        canvasText.setVisibility(View.INVISIBLE);
+
+        if(Build.VERSION.SDK_INT>=21)
+            SplashActivity.TTSobj.speak(practice_string,
+                TextToSpeech.QUEUE_FLUSH,  // Drop all pending entries in the playback queue.
+                null, null);
+        else
+            SplashActivity.TTSobj.speak(practice_string,
+                    TextToSpeech.QUEUE_FLUSH,  // Drop all pending entries in the playback queue.
+                    null);
     }
 
     private Bitmap takeScreenshot(int ResourceID) {
@@ -35,9 +53,12 @@ public class PracticeActivity extends Activity {
         //selecting the part to be saved
         v1.layout(0, 0, v1.getMeasuredWidth(), v1.getMeasuredHeight());
         v1.setDrawingCacheEnabled(true);
-        final Bitmap bitmap = Bitmap.createBitmap(v1.getDrawingCache()); // actually taking the screen shot
+        Bitmap bitmap = Bitmap.createBitmap(v1.getDrawingCache()); // actually taking the screen shot
         v1.setDrawingCacheEnabled(false);
         return bitmap;
     }
 
+    public void clear(View v) {
+        drawView.setBitmap(drawViewBitmap);
+    }
 }

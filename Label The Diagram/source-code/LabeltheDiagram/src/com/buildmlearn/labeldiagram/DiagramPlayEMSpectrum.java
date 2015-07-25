c3962ca6843a6077ccb7d2c915ca9deb74145c02
Akila <sandakelum.akila@gmail.com>
2015-07-25 19:40:49 +0530

    code refactoring

---
+++ Label The Diagram/source-code/LabeltheDiagram/src/com/buildmlearn/labeldiagram/DiagramPlayEMSpectrum.java
@@ -0,0 +1,23 @@
+package com.buildmlearn.labeldiagram;
+
+import android.os.Bundle;
+
+public class DiagramPlayEMSpectrum extends DiagramPlayBase{
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}	@Override
+	protected void dispatch(float totalScore, int gameScore) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected int getResourcesId() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}

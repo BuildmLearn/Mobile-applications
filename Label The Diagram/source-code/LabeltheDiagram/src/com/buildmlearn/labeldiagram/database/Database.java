64f7196a633683d34e446e91c5e8e56d82808f2e
Akila <sandakelum.akila@gmail.com>
2015-08-09 21:54:07 +0530

    Updated database for badges data

--- Label The Diagram/source-code/LabeltheDiagram/src/com/buildmlearn/labeldiagram/database/Database.java
+++ Label The Diagram/source-code/LabeltheDiagram/src/com/buildmlearn/labeldiagram/database/Database.java
@@ -8,7 +8,7 @@ import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
 
 public class Database extends SQLiteAssetHelper{
 	
-	private static final String DATABASE_NAME = "lessons.db";
+	private static final String DATABASE_NAME = "staticdata.db";
     private static final int DATABASE_VERSION = 1;
     private SQLiteDatabase db;
 

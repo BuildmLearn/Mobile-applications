c74610d777c4a469f2c60ac2fff7b22a5ed1567f
Akila <sandakelum.akila@gmail.com>
2015-08-15 23:32:52 +0530

    Added help menu screens

---
+++ Label The Diagram/source-code/LabeltheDiagram/src/com/buildmlearn/labeldiagram/HelpMenu.java
@@ -0,0 +1,14 @@
+package com.buildmlearn.labeldiagram;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HelpMenu extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+	
+}

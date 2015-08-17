262493eddbc4d18113ebe5181c3da732016ff669
Akila <sandakelum.akila@gmail.com>
2015-08-17 14:48:49 +0530

    Fixed collision in normal layout for diagram result

--- Label The Diagram/source-code/LabeltheDiagram/src/com/buildmlearn/labeldiagram/SplashScreen.java
+++ Label The Diagram/source-code/LabeltheDiagram/src/com/buildmlearn/labeldiagram/SplashScreen.java
@@ -20,7 +20,7 @@ public class SplashScreen extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-		Fabric.with(this, new Crashlytics());
+		//Fabric.with(this, new Crashlytics());
         setContentView(R.layout.splash_screen);
         
         // Calling NavigationHandler to wait and dispatch to MainMenu
